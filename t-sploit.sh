2c70d8397258f5d9672c66e261c1973f3f709e66
rootexx <velies0625@gmail.com>
2023-05-11 12:59:51 +0300

    Update t-sploit.sh

--- t-sploit.sh
+++ t-sploit.sh
@@ -4,56 +4,20 @@
 clear
 
 run_sqlinj() {
-    echo "SQL İnjection çalıştırılıyor..."
+    echo "sqlinj dosyası çalıştırılıyor..."
     python sqlinj/sqlmap.py
 }
 
 run_ddos() {
-    echo "DDoS çalıştırılıyor..."
-    python ddos.py
+    echo "ddos.py dosyası çalıştırılıyor..."
+    python ddos/ddos.py
 }
 
 run_phishing() {
-    echo "phishing çalıştırılıyor..."
+    echo "zpisher.sh dosyası çalıştırılıyor..."
     chmod +x phishing/zpisher.sh
     ./phishing/zpisher.sh
 }
-
-case $1 in
-    "--help")
-        sleep 1
-        echo -e "
---help & --h   (Yardım Menüsünü Çağırır)
---ıp & --ip    (Giriş Yapmış Kişilerin Ip Adresini Listeler)
---user         (Giriş Yapılmış Username & Pass)
---mac          (Giriş Yapanların Mac Adresleri)
---location     (Belirtilen Ip Adresinin Yer Tespiti)
---ddos         (DDoS Saldırısı Başlatır)
---phishing     (Phishing Saldırısı Başlatır)
-        "
-        ;;
-    "--h")
-        sleep 1
-        echo -e "
---help & --h   (Yardım Menüsünü Çağırır)
---ıp & --ip    (Giriş Yapmış Kişilerin Ip Adresini Listeler)
---user         (Giriş Yapılmış Username & Pass)
---mac          (Giriş Yapanların Mac Adresleri)
---location     (Belirtilen Ip Adresinin Yer Tespiti)
---ddos         (DDoS Saldırısı Başlatır)
---phishing     (Phishing Saldırısı Başlatır)
-        "
-        ;;
-    "sqlinj")
-        run_sqlinj
-        ;;
-    "ddos")
-        run_ddos
-        ;;
-    "phishing")
-        run_phishing
-        ;;
-esac
 
 banner() {
     echo -e '
@@ -65,10 +29,32 @@ banner() {
         ██████╔╝██║░░██║██║░╚██╗██║██║░╚███║██████╔╝░░░██║░░░
         ╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝
     '
+    echo -e '
+\033[0m[1] SQL Injection
+[2] DDoS
+[3] Phishing
+[4] Exit
+    '
+    read -p "Select an option: " choice
+    case $choice in
+        1)
+            run_sqlinj
+            ;;
+        2)
+            run_ddos
+            ;;
+        3)
+            run_phishing
+            ;;
+        4)
+            exit 0
+            ;;
+        *)
+            echo "Invalid option. Please select a valid option."
+            ;;
+    esac
 }
 
-banner
-
-read -p "İşlem Numarası: " islem
-
-# İşlemi değerlendir ve il
+while true; do
+    banner
+done
